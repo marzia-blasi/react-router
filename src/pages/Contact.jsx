1eb8c282daffaa19275676fc8c46781b8d0f012a
marzia <you@example.com>
2025-07-24 17:59:00 +0200

    add test inputs for the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,72 @@
 export default function Contact() {
   return (
     <>
-      <main>Provole ed altre cose </main>
+      <main>
+        <div className="container-light">
+          <form action="">
+            <div className="mb-3">
+              <label htmlFor="" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name=""
+                id=""
+                aria-describedby="helpId"
+                placeholder=""
+              />
+              <small id="helpId" className="form-text text-muted">
+                Help text
+              </small>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="" className="form-label">
+                City
+              </label>
+              <select className="form-select form-select-lg" name="" id="">
+                <option selected="">Select one</option>
+                <option value="">New Delhi</option>
+                <option value="">Istanbul</option>
+                <option value="">Jakarta</option>
+              </select>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                defaultValue=""
+                id=""
+              />
+              <label className="form-check-label" htmlFor="">
+                Default checkbox
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                defaultValue=""
+                id=""
+                defaultChecked=""
+              />
+              <label className="form-check-label" htmlFor="">
+                Default checkbox
+              </label>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="" className="form-label" />
+              <textarea
+                className="form-control"
+                name=""
+                id=""
+                rows={3}
+                defaultValue={""}
+              />
+            </div>
+          </form>
+        </div>
+      </main>
     </>
   );
 }
